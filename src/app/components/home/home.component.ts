5eb7fd1d9bfae5647d0946eec36ad2c5a084d67b
pevargasg <wearetamo@gmail.com>
2019-03-17 10:33:51 -0700

    drag and drop working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  dragged: boolean;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  allowDrop(ev) {
+    ev.preventDefault();
   }
 
+  drag(ev) {
+    this.dragged = true;
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  drop(ev: any, id: string) {
+    if (!document.getElementById(id).hasChildNodes()) {
+      this.dragged = false;
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    } else {
+      this.dragged = false;
+    }
+  }
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatIconModule } from "@angular/material/icon";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
-  imports: [MatBadgeModule, MatIconModule],
-  exports: [MatBadgeModule, MatIconModule]
+  imports: [MatBadgeModule, MatIconModule, DragDropModule],
+  exports: [MatBadgeModule, MatIconModule, DragDropModule]
 })
 export class MaterialModule {}
