0103d75f005ae07a8d8a3c37503e709146977a81
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-03-18 16:00:14 -0400

    pulling days for forecast

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from 'src/app/shared/services/weather.service';
-
+import { pipe } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
-
+  currentTime: number;
+  forecast: [];
   constructor(private _weatherService: WeatherService) { }
 
   ngOnInit() {
-    this.getWeatherInfo();
+    this.getCurrentWeatherInfo();
   }
 
-  getWeatherInfo() {
-    this._weatherService.getDaysForecast().subscribe(res => {
-      console.log(res);
-    });
+
+  getCurrentWeatherInfo() {
+    this._weatherService.getCurrentWeather()
+     .pipe(
+       finalize(() => {
+        this._weatherService.getForecastWeather().pipe(finalize(() => {
+          console.log(this.forecast);
+        })).subscribe(fc => {
+          this.forecast = this.getDaysForecast(fc);
+        });
+       })).subscribe(cw => {
+         this.currentTime = new Date(cw['dt']).getHours();
+       });
+  }
+
+  getDaysForecast(forecast): []  {
+    return forecast['list'].reduce((acc, val) => {
+      if (val['dt_txt'].split(' ')[1].substring(0, 2 ) == this.currentTime) {
+         acc.push(val);
+      }
+      return acc;
+    }, []);
   }
 
 }
--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { reduce } from 'rxjs/operators';
 import * as _ from 'lodash';
 
 @Injectable({
@@ -9,22 +9,15 @@ import * as _ from 'lodash';
 export class WeatherService {
   API_KEY = '39dd6312d1d9c6ab34ba092ee65ffd29';
   LOCATION = 'Jacksonville,US';
-  BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+  BASE_URL = 'http://api.openweathermap.org/data/2.5/';
 
   constructor(private http: HttpClient) { }
 
-  getWeather() {
-     return this.http.get(this.BASE_URL + this.LOCATION + '&units=metric&appid=' + this.API_KEY);
+  getForecastWeather() {
+     return this.http.get(this.BASE_URL + 'forecast?q=' + this.LOCATION + '&units=metric&appid=' + this.API_KEY);
   }
 
-  getDaysForecast() {
-    return this.getWeather().pipe(
-        map(f => {
-        f['list'].forEach(d => {
-          d.dt_txt = d.dt_txt.substring(0, 10);
-        });
-          return _.groupBy(f['list'], 'dt_txt');
-       })
-     );
+  getCurrentWeather() {
+    return this.http.get(this.BASE_URL + 'weather?q=' + this.LOCATION + '&units=metric&appid=' + this.API_KEY);
   }
 }
