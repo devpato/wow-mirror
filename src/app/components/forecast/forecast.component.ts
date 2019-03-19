c8a55251fa5b10733a106fa4bd8ed5ba10b55b25
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-03-19 02:56:50 -0400

    forecast 90% done

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as iconTable from '../../shared/const/icons';
+import { WeatherService } from 'src/app/shared/services/weather.service';
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
-
-  constructor() { }
+  currentTime: number;
+  forecast = [];
+  currentWeather: any;
+  wind: number;
+  sunset: Date;
+  sunrise: Date;
+  icons = iconTable;
+  constructor(private _weatherService: WeatherService) {}
 
   ngOnInit() {
+    this.getCurrentWeatherInfo();
   }
+
+
+  getCurrentWeatherInfo() {
+    this._weatherService.getCurrentWeather()
+     .pipe(
+       finalize(() => {
+        this._weatherService.getForecastWeather().subscribe(fc => {
+          this.forecast = this.getDaysForecast(fc);
+        });
+       })).subscribe(cw => {
+         this.currentTime = new Date(cw['dt'] * 1000).getHours();
+       });
+  }
+
+  getDaysForecast(forecast): any[]  {
+    return forecast['list'].reduce((acc, val) => {
+      const TEMP_TIME = Number(val['dt_txt'].split(' ')[1].substring(0, 2 ));
+      if ( TEMP_TIME === this.currentTime || (TEMP_TIME >= this.currentTime && TEMP_TIME <= this.currentTime + 3)) {
+         acc.push(val);
+      }
+      return acc;
+    }, []);
+  }
+
+  getDate(date: number) {
+    return new Date(date * 1000);
+  }
+
 
 }
--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from 'src/app/shared/services/weather.service';
 import { pipe } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 import * as iconTable from '../../shared/const/icons';
 @Component({
   selector: 'app-weather',
@@ -10,7 +9,6 @@ import * as iconTable from '../../shared/const/icons';
 })
 export class WeatherComponent implements OnInit {
   currentTime: number;
-  forecast = [];
   currentWeather: any;
   wind: number;
   sunset: Date;
@@ -23,32 +21,13 @@ export class WeatherComponent implements OnInit {
     this.getCurrentWeatherInfo();
   }
 
-
   getCurrentWeatherInfo() {
-    this._weatherService.getCurrentWeather()
-     .pipe(
-       finalize(() => {
-        this._weatherService.getForecastWeather().subscribe(fc => {
-          this.forecast = this.getDaysForecast(fc);
-        });
-       })).subscribe(cw => {
-         this.currentTime = new Date(cw['dt'] * 1000).getHours();
-         this.currentWeather = cw;
-         this.wind = Math.round(this.currentWeather.wind.speed);
-         this.sunrise = new Date(this.currentWeather.sys.sunrise * 1000);
-         this.sunset = new Date(this.currentWeather.sys.sunset * 1000);
-         console.log(cw);
-       });
+    this._weatherService.getCurrentWeather().subscribe(cw => {
+      this.currentTime = new Date(cw['dt'] * 1000).getHours();
+      this.currentWeather = cw;
+      this.wind = Math.round(this.currentWeather.wind.speed);
+      this.sunrise = new Date(this.currentWeather.sys.sunrise * 1000);
+      this.sunset = new Date(this.currentWeather.sys.sunset * 1000);
+    });
   }
-
-  getDaysForecast(forecast): any[]  {
-    return forecast['list'].reduce((acc, val) => {
-      const TEMP_TIME = Number(val['dt_txt'].split(' ')[1].substring(0, 2 ));
-      if ( TEMP_TIME === this.currentTime || (TEMP_TIME > TEMP_TIME && TEMP_TIME <= this.currentTime + 3)) {
-         acc.push(val);
-      }
-      return acc;
-    }, []);
-  }
-
 }
