fcc41e1437027491344293d9d96083ef466fc823
pevargasg <wearetamo@gmail.com>
2019-03-21 02:41:19 -0700

    fix date issues

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService } from 'src/app/shared/services/weather.service';
-import { pipe } from 'rxjs';
-import * as iconTable from '../../shared/const/icons';
+import { Component, OnInit } from "@angular/core";
+import { WeatherService } from "src/app/shared/services/weather.service";
+import { pipe } from "rxjs";
+import * as iconTable from "../../shared/const/icons";
+import * as moment from "moment-timezone";
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  selector: "app-weather",
+  templateUrl: "./weather.component.html",
+  styleUrls: ["./weather.component.scss"]
 })
 export class WeatherComponent implements OnInit {
-  currentTime: number;
+  currentTime: string;
   currentWeather: any;
   wind: number;
   sunset: Date;
@@ -16,18 +17,32 @@ export class WeatherComponent implements OnInit {
   icons = iconTable;
   constructor(private _weatherService: WeatherService) {}
 
-
   ngOnInit() {
     this.getCurrentWeatherInfo();
   }
 
   getCurrentWeatherInfo() {
     this._weatherService.getCurrentWeather().subscribe(cw => {
-      this.currentTime = new Date(cw['dt'] * 1000).getHours();
+      this.currentTime = moment
+        .utc(new Date(cw["dt"] * 1000), "YYYY-MM-DD HH:mm:ss")
+        .tz("America/New_York")
+        .format("H");
       this.currentWeather = cw;
       this.wind = Math.round(this.currentWeather.wind.speed);
-      this.sunrise = new Date(this.currentWeather.sys.sunrise * 1000);
-      this.sunset = new Date(this.currentWeather.sys.sunset * 1000);
+      this.sunrise = moment
+        .utc(
+          new Date(this.currentWeather.sys.sunrise * 1000),
+          "YYYY-MM-DD HH:mm:ss"
+        )
+        .tz("America/New_York")
+        .format("HH:mma");
+      this.sunset = moment
+        .utc(
+          new Date(this.currentWeather.sys.sunset * 1000),
+          "YYYY-MM-DD HH:mm:ss"
+        )
+        .tz("America/New_York")
+        .format("HH:mma");
     });
   }
 }
